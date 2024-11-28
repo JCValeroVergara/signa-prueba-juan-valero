1e27f69944514e3141efca4bac2a1b82d888bf2a
Juank_Valero <jvalero2009@gmail.com>
2024-11-27 20:29:13 -0500

    Add Tailwind CSS and PostCSS configuration; update app structure and styles

---
+++ client/src/data/index.js
@@ -0,0 +1,8 @@
+
+export const marcas = [
+    { id: 1, nombre: 'Audi', titular: 'Volkswagen', Estado: 'Alemania' },
+    { id: 2, nombre: 'BMW', titular: 'BMW', Estado: 'Alemania' },
+    { id: 3, nombre: 'Chevrolet', titular: 'General Motors', Estado: 'Estados Unidos' },
+    { id: 4, nombre: 'Citroën', titular: 'Groupe PSA', Estado: 'Francia' },
+    { id: 5, nombre: 'Fiat', titular: 'Fiat Chrysler Automobiles', Estado: 'Italia' },
+];
